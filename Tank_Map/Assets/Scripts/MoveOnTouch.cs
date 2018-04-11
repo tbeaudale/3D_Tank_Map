130aa1337bf8960115b5641f94f7732b034834ea
Tom <tbeaudale@gmail.com>
2018-04-11 11:01:56 +0100

    sorted

---
+++ Tank_Map/Assets/Scripts/MoveOnTouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnTouch : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 velocity;
+
+    private bool moving;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        moving = true;
+        collision.collider.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        moving = true; 
+        collision.collider.transform.SetParent(null);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (moving && transform.position.x <= -313.3)
+        {
+            moving = false;
+        }
+        if (moving)
+        {
+            transform.position += (velocity * Time.deltaTime);
+        }
+	}
+}
